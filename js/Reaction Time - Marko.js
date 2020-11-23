b8ea50d65fca931fe4ebca81a01ef4280ae2d413
James M <james1320McCune@gmai.com>
2020-11-23 11:55:22 -0700

    Anti Spam

--- js/Reaction Time - Marko.js
+++ js/Reaction Time - Marko.js
@@ -1,10 +1,12 @@
 //sets the timer to a random time at which to change the background color
-var timer = (Math.random() * (7) ) + 2;
+var timer = (Math.random() * (7) ) + 1;
  
  
 var result = false;
 var time = 0;
- 
+var early = false;
+stop = false;
+stop2 = false;
  
 function setup() {
   var cnv = createCanvas(1150, 600);
@@ -29,50 +31,86 @@ function mouseClicked()
 }
  
  
+ 
 function reset()
 {
 	timer = (Math.random() * (7) ) + 2;
 	result = false;
 	time = 0;
-	
+  early = false;
+	stop2 = false;
 	if (mouseButton == LEFT)
     {
         button.hide();
     }
 }
  
+function mousePressed()
+{
+  if(stop2 == false)
+    {
+  timer = (Math.random() * (7) ) + 2;
+  stop = true;
+ 
+    }
+ 
+}
  
 function resetButton()
 {	
-
+ 
 	if (typeof button == "undefined")
 	{
 		button = createButton('reset');
 		button.position(600 , 465);
+        button.size(100,40);
+        button.style("font-size", 56);
+ 
+ 
+ 
+ 
 		button.mousePressed(reset);
 	}
-	
+ 
 	else
 	{
 		button.show();
 	}
 }
-
+ 
 function game()
 {
  
   background(95,240,240);
-  
+ 
  //title
   stroke(0);
   strokeWeight(4);
   fill('white')
   rect(50,35, 1050, 100, 20);
-  
+ 
   strokeWeight(0);
   fill('black')
   textSize(45);
   text('Click Your Mouse When the Screen Changes Color',60,100);
+ 
+  //warning message
+  stroke(0);
+  strokeWeight(4);
+  fill('white')
+  rect(300,360, 555, 150, 20);
+ 
+ 
+ strokeWeight(0);
+  fill('black')
+  textSize(30);
+  text('The screen will not change colors as',335,427);
+ 
+  strokeWeight(0);
+  fill('black')
+  textSize(30);
+  text('long as you are clicking too early',355,465);
+ 
  
 //timer
 if (timer > 0)
@@ -89,14 +127,15 @@ else
  if(timer < 0)
 {
 	background(50,200,65);
-    
+  stop2 = true;
+ 
 	if (time >= 0)
 	{
 		time = time + (1/60);
 	}
 }
  
-  
+ 
 if(result == true && timer < 0)
   {
  
@@ -105,16 +144,16 @@ if(result == true && timer < 0)
 	strokeWeight(4);
 	fill('white')
 	rect(50,35, 1050, 100, 20);
-    
+ 
 	strokeWeight(0);
 	fill('black');
 	textSize(40);
 	text('It Took You: ' + round(time, 5) + ' Seconds to Click Your Mouse',100,100);
-  
+ 
 	time = time - (1/60);
-    
+ 
 	resetButton();
-	
+ 
   } 
  
 }
